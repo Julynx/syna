697f6cf7c5b9cae2932163bc7310d72e12d3240f
Julynx <julicabria@outlook.com>
2026-09-15 01:37:27 +0200

    made Syna more capable

--- src/syna/state.py
+++ src/syna/state.py
@@ -16,7 +16,7 @@ def get_container():
         init_docker_client()
     if container is not None:
         return container
-    container = client.run_container("debian:bookworm-slim", command="sleep infinity")
+    container = client.run_container("python:3.14-trixie", command="sleep infinity")
     return container
 
 
